540595fd7881f3d510e3f93dff672428bd3a12ad
nastya shishkina <ana.koze@yandex.ru>
2023-11-02 14:53:42 +0400

    добавленa функция str_title, добавлены докстринги

--- str_func.py
+++ str_func.py
@@ -1,4 +1,10 @@
 def str_upper(data:str):
+    """делает буквы заглавными"""
     data_up = data.capitalize()
     return data_up
 
+
+def str_title(data:str):
+    """делает первую букву в каждом слове заглавной"""
+    data_title = data.title()
+    return data_title
